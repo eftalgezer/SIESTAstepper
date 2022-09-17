508d1bc2776760919fc0c1298a8add5119314896
Eftal Gezer <egezer@localhost.localdomain>
2022-09-17 03:26:48 +0300

    Update tests

---
+++ tests/__main__.py
@@ -0,0 +1,8 @@
+"""
+Evaluate unit tests for the SIESTAstepper library.
+"""
+from .tests import *
+
+ani_to_fdf_test()
+xyz_to_fdf_test()
+clear_temp()
---
+++ tests/tests.py
@@ -0,0 +1,49 @@
+"""
+Unit tests for the SIESTAstepper library.
+"""
+import os
+import shutil
+from SIESTAstepper import __file__ as mfile
+from .testers import *
+
+mpath = mfile.replace("/SIESTAstepper/__init__.py", "")
+
+
+def ani_to_fdf_test():
+    """Tests for ani_to_fdf"""
+    assert ani_to_fdf_tester(
+        f"{mpath}{os.sep}tests{os.sep}assets{os.sep}ANI{os.sep}C-4.ANI",
+        f"{mpath}{os.sep}tests{os.sep}assets{os.sep}fdf{os.sep}C-0.fdf",
+        f"{mpath}{os.sep}tests{os.sep}assets{os.sep}temp{os.sep}C-5.fdf"
+    ) == read_file(f"{mpath}{os.sep}tests{os.sep}assets{os.sep}fdf{os.sep}C-5.fdf")
+
+
+def xyz_to_fdf_test():
+    """Tests for xyz_to_fdf"""
+    assert xyz_to_fdf_tester(
+        f"{mpath}{os.sep}tests{os.sep}assets{os.sep}xyz{os.sep}C.xyz",
+        f"{mpath}{os.sep}tests{os.sep}assets{os.sep}fdf{os.sep}C-0.fdf",
+        f"{mpath}{os.sep}tests{os.sep}assets{os.sep}temp{os.sep}C-1.fdf"
+    ) == read_file(f"{mpath}{os.sep}tests{os.sep}assets{os.sep}fdf{os.sep}C-1.fdf")
+
+
+def merge_ani_test():
+    """Tests for merge_ani"""
+    assert merge_ani_tester(label="C", folder="Carbon") == read_file(
+        f"{mpath}{os.sep}tests{os.sep}assets{os.sep}ANI{os.sep}C-merged.ANI"
+    )
+
+def make_directories_test():
+    """Tests for make_directories"""
+    assert make_directories_tester(5) == ["i1", "i2", "i3", "i4", "i5"]
+def clear_temp():
+    """Clears the temp folder"""
+    for filename in os.listdir(f"{mpath}{os.sep}tests{os.sep}assets{os.sep}temp"):
+        filepath = os.path.join(f"{mpath}{os.sep}tests{os.sep}assets{os.sep}temp", filename)
+        try:
+            if os.path.isfile(filepath) or os.path.islink(filepath):
+                os.unlink(filepath)
+            elif os.path.isdir(filepath):
+                shutil.rmtree(filepath)
+        except Exception as e:
+            print(f'Failed to delete {filepath}. Reason: {e}')
