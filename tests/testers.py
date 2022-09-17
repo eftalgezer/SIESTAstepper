46e5cfc4d1e9d39894cbd23be159c2744156ac1d
Eftal Gezer <eftalgezer@gmail.com>
2022-09-17 04:46:39 +0300

    Add files via upload

--- tests/testers.py
+++ tests/testers.py
@@ -54,7 +54,7 @@ def tester_merge_ani(label=None, path=None, folder=None):
 
 def tester_make_directories(n):
     """Tester function for make_directories"""
-    update_cwd("{mpath}{os.sep}tests{os.sep}temp")
+    update_cwd(f"{mpath}{os.sep}tests{os.sep}temp")
     make_directories(n)
     return list(glob.glob("i*"))
 
