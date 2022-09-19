14e84242c32540dd5795f2cecccf765401ac7026
Eftal Gezer <eftalgezer@gmail.com>
2022-09-20 02:15:38 +0300

    Add files via upload

--- tests/tests.py
+++ tests/tests.py
@@ -434,9 +434,7 @@ def test_carbon_uninterrupted_project():
         f"{mpath}{os.sep}tests{os.sep}assets{os.sep}temp{os.sep}{get_fake_project()}{os.sep}i4",
         f"{mpath}{os.sep}tests{os.sep}assets{os.sep}temp{os.sep}{get_fake_project()}{os.sep}i5"
     ]
-    assert copy_files_tester(["psf"], "C", ".", "i1") == [
-        f"{mpath}{os.sep}tests{os.sep}assets{os.sep}temp{os.sep}{get_fake_project()}{os.sep}i1{os.sep}C.psf"
-    ]
+    assert copy_files_tester(["psf"], "C", ".", "i1") == [f"i1{os.sep}C.psf"]
     assert xyz_to_fdf_tester("C.xyz", "C.fdf", f"i1{os.sep}C.fdf") == read_file(
         f"{mpath}{os.sep}tests{os.sep}assets{os.sep}runs{os.sep}{get_fake_project()}{os.sep}i1{os.sep}C.fdf"
     )
