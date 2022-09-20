29bd194e8a625151446e9f6df7be925e3be9f76d
Eftal Gezer <eftalgezer@gmail.com>
2022-09-20 03:33:09 +0300

    Add files via upload

--- tests/testers.py
+++ tests/testers.py
@@ -6,7 +6,7 @@ import os
 import shutil
 import io
 import sys
-from _pytest.monkeypatch import MonkeyPatch
+import types
 from SIESTAstepper import __file__ as mfile
 from SIESTAstepper.core import run, single_run, run_next, run_interrupted, single_run_interrupted, make_directories, \
     copy_files, ani_to_fdf, xyz_to_fdf, merge_ani, analysis, energy_diff, _command, contfiles, contextensions, \
@@ -49,9 +49,8 @@ def initialise_fake_project():
             shutil.copy(f, f"{mpath}{os.sep}tests{os.sep}assets{os.sep}temp{os.sep}{fakeproject}{os.sep}{fname}")
 
 
-def fake_command(label=None, issingle=False, monkeypatch=None):
+def _command(label=None, issingle=False):
     """A fake SIESTA run command"""
-    monkeypatch.setattr("_command", fake_command)
     realpath = os.getcwd().replace(f"{os.sep}temp{os.sep}", f"{os.sep}runs{os.sep}")
     with open(f"{realpath}{os.sep}{log}", "r") as reallog:
         with open(f"{os.getcwd()}{os.sep}{log}", "w") as fakelog:
