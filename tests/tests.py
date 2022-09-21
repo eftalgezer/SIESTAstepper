d5115f7bc0874a0dfd32f7e853c6e19a65057ed7
Eftal Gezer <eftalgezer@gmail.com>
2022-09-22 00:06:41 +0300

    Add files via upload

--- tests/tests.py
+++ tests/tests.py
@@ -508,10 +508,10 @@ def test_carbon_project_single_run_interrupted():
 
 def test_main():
     set_cwd(f"{mpath}{os.sep}tests{os.sep}assets{os.sep}temp")
-    assert f"{mpath}{os.sep}tests{os.sep}assets{os.sep}temp{os.sep}C-5.fdf is created" in main_tester(
+    assert main_tester(
         f"SIESTAstepper" +
-        " ani_to_fdf" +
+        f" ani_to_fdf" +
         f" {mpath}{os.sep}tests{os.sep}assets{os.sep}ANI{os.sep}C-4.ANI" +
         f" {mpath}{os.sep}tests{os.sep}assets{os.sep}fdf{os.sep}C-0.fdf" +
         f" {mpath}{os.sep}tests{os.sep}assets{os.sep}temp{os.sep}C-5.fdf"
-    )
+    ).find(f"{mpath}{os.sep}tests{os.sep}assets{os.sep}temp{os.sep}C-5.fdf is created") > -1
