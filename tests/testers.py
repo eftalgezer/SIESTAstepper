96d4e9fece7dcbf8bc23aafaa4abed74a640b22e
Eftal Gezer <eftalgezer@gmail.com>
2022-09-21 23:50:21 +0300

    Add files via upload

--- tests/testers.py
+++ tests/testers.py
@@ -317,10 +317,7 @@ def remove_nones_tester(files, path, cwd, cont, log):
 
 def main_tester(command):
     fake_command()
-    fakeargs = []
-    fakeargs.extend(command.split(" "))
-    print(fakeargs)
-    with patch('sys.argv', fakeargs):
+    with patch('sys.argv', command.split(" ")):
         from SIESTAstepper import __main__ as rtmain
         capturedoutput = io.StringIO()
         sys.stdout = capturedoutput
