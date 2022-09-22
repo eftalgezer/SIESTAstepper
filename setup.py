15865768e441123f3f86defc8cdc526f2c80ea38
Eftal Gezer <eftalgezer@gmail.com>
2022-09-22 22:55:24 +0300

    Linting

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ import pathlib
 from setuptools import setup
 
 
-here = pathlib.Path(__file__).parent.resolve()
+HERE = pathlib.Path(__file__).parent.resolve()
 
-long_description = (here / "README.md").read_text(encoding="utf-8")
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="SIESTAstepper",
     version="2.0.0",
     description="SIESTAstepper runs SIESTA step by step, designed for constrained calculations.",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/eftalgezer/SIESTAstepper",
     author="Eftal Gezer",
