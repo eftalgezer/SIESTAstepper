236a6f2b3093178eff9f4b4e3a1b9bcdb17083d7
Eftal Gezer <eftalgezer@gmail.com>
2022-09-23 19:24:42 +0300

    Add files via upload

--- tests/tests.py
+++ tests/tests.py
@@ -637,7 +637,7 @@ def main_carbon_uninterrupted_project_run_next():
     assert energy_difftest is not None or energy_difftest != ""
 
 
-def test_main_carbon_uninterrupted_project_single_run():
+def main_carbon_uninterrupted_project_single_run():
     """Run tests based on Carbon_uninterrupted run single_run function with __main__.py"""
     fake_command()
     set_fake_project("Carbon_uninterrupted")
