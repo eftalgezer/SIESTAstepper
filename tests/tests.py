085fb74e20296ce0bd7ea6324f6ded4dc624f053
Eftal Gezer <eftalgezer@gmail.com>
2022-09-26 00:29:40 +0300

    Add files via upload

--- tests/tests.py
+++ tests/tests.py
@@ -701,7 +701,7 @@ def test_main_carbon_uninterrupted_project_single_run():
     assert single_runtest is not None or single_runtest != ""
 
 
-def test_main_carbon_project_run_interrupted():
+def main_carbon_project_run_interrupted():
     """Run tests based on Carbon_uninterrupted run run_interrupted function with __main__.py"""
     fake_command()
     set_fake_project("Carbon")
