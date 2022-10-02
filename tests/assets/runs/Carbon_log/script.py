693dd35acab6c88d453243b733077f24753415cc
Eftal Gezer <eftalgezer@gmail.com>
2022-10-02 23:06:53 +0300

    Delete script.py

--- tests/assets/runs/Carbon_log/script.py
+++
@@ -1,9 +0,0 @@
-import SIESTAstepper
-import os
-SIESTAstepper.settings.set_cwd(os.getcwd())
-#SIESTAstepper.conda = "envir" #to work with Anaconda
-#SIESTAstepper.cores = 4 #to set number of cores for parallel run
-#SIESTAstepper.make_directories(5) #to create folder if you are not created at first
-SIESTAstepper.copy_files(["psf"], "C", ".", "i1")
-SIESTAstepper.xyz_to_fdf("C.xyz", "C.fdf", "i1/C.fdf")
-SIESTAstepper.run("C")
