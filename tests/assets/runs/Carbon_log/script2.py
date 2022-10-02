e3d1c64a7ce1da666a17d9f11497424cd69c8359
Eftal Gezer <eftalgezer@gmail.com>
2022-10-02 23:07:00 +0300

    Delete script2.py

--- tests/assets/runs/Carbon_log/script2.py
+++
@@ -1,6 +0,0 @@
-import SIESTAstepper
-import os
-SIESTAstepper.settings.set_cwd(os.getcwd())
-SIESTAstepper.settings.set_cont("continue") # default is "continue" 
-SIESTAstepper.settings.contfiles.append("pair")
-SIESTAstepper.run_interrupted("3", "C")
