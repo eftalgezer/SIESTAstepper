1ab9b52f991e8a209f09d34a876a92271d7f9031
Eftal Gezer <eftalgezer@gmail.com>
2022-10-04 03:07:12 +0300

    Backwards compatibility

--- SIESTAstepper/helpers.py
+++ SIESTAstepper/helpers.py
@@ -1,10 +1,10 @@
+# coding=utf-8
 """
 Helper functions for SIESTA runs or analysis of SIESTA log files
 """
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import with_statement
-from __future__ import unicode_literals
 import os
 import re
 import shutil
